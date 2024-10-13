d1d06bfb76b6f6c7d7855f0ca61ca3dffb9cf7af
Kautuk Dwivedi <kautukdwivedi@gmail.com>
2024-10-13 19:53:44 +0300

    Added support for * entry id arg in XADD command.

--- app/stream.go
+++ app/stream.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 var (
@@ -51,61 +52,58 @@ func (s *Stream) NewStreamEntry(id string) (*StreamEntry, error) {
 }
 
 func (s *Stream) NewStreamEntryId(id string) (*StreamEntryId, error) {
-	pieces := strings.Split(id, "-")
-
-	if len(pieces) != 2 {
-		return nil, ErrInvalidStreamEntrId
-	}
-
 	var millisTime int
 	var sequenceNr int
 
-	if pieces[0] == "*" {
-		if s.isEmpty() {
-			millisTime = 0
-		} else {
-			millisTime = s.lastEntry().ID.MillisTime + 1
-		}
+	if id == "*" {
+		millisTime = int(time.Now().UTC().UnixMilli())
+		sequenceNr = 0
 	} else {
+		pieces := strings.Split(id, "-")
+
+		if len(pieces) != 2 {
+			return nil, ErrInvalidStreamEntrId
+		}
+
 		ms, err := strconv.Atoi(pieces[0])
 		if err != nil {
 			return nil, ErrInvalidStreamEntrId
 		}
 		millisTime = ms
-	}
 
-	if pieces[1] == "*" {
-		if s.isEmpty() {
-			sequenceNr = 1
-		} else {
-			matchingEntry := s.findEntryByMillis(millisTime)
-			if matchingEntry == nil {
-				sequenceNr = 0
+		if pieces[1] == "*" {
+			if s.isEmpty() {
+				sequenceNr = 1
 			} else {
-				sequenceNr = matchingEntry.ID.SequenceNr + 1
+				matchingEntry := s.findEntryByMillis(millisTime)
+				if matchingEntry == nil {
+					sequenceNr = 0
+				} else {
+					sequenceNr = matchingEntry.ID.SequenceNr + 1
+				}
 			}
-		}
-	} else {
-		nr, err := strconv.Atoi(pieces[1])
-		if err != nil {
-			return nil, ErrInvalidStreamEntrId
-		}
-		sequenceNr = nr
-	}
-
-	if millisTime <= 0 && sequenceNr <= 0 {
-		return nil, ErrStreamEntryIdSmallerThanZero
-	}
-
-	if !s.isEmpty() {
-		lastEntryId := s.lastEntry().ID
-
-		if lastEntryId.MillisTime > millisTime {
-			return nil, ErrStreamEntryIdEqualOrSmallerThanTopItem
+		} else {
+			nr, err := strconv.Atoi(pieces[1])
+			if err != nil {
+				return nil, ErrInvalidStreamEntrId
+			}
+			sequenceNr = nr
 		}
 
-		if lastEntryId.MillisTime == millisTime && lastEntryId.SequenceNr >= sequenceNr {
-			return nil, ErrStreamEntryIdEqualOrSmallerThanTopItem
+		if millisTime <= 0 && sequenceNr <= 0 {
+			return nil, ErrStreamEntryIdSmallerThanZero
+		}
+
+		if !s.isEmpty() {
+			lastEntryId := s.lastEntry().ID
+
+			if lastEntryId.MillisTime > millisTime {
+				return nil, ErrStreamEntryIdEqualOrSmallerThanTopItem
+			}
+
+			if lastEntryId.MillisTime == millisTime && lastEntryId.SequenceNr >= sequenceNr {
+				return nil, ErrStreamEntryIdEqualOrSmallerThanTopItem
+			}
 		}
 	}
 
